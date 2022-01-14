ab4fdff68a58f15099a04bacc92a100713f6ca87
Alexis Newby <alexis.newby@gmail.com>
2022-01-14 07:46:42 -0700

    Build tests for Customer class constructor

---
+++ test/Customer-test.js
@@ -0,0 +1,35 @@
+import { expect } from 'chai';
+import Customer from '../src/classes/Customer';
+import sampleCustomers from'../src/data/sampleCustomers';
+
+const getRandomIndex = (array) => {
+  return array[Math.floor(Math.random() * array.length)];
+};
+
+describe('Customer', () => {
+    let customer;
+
+    beforeEach(() => {
+      customer = new Customer(getRandomIndex(smapleCustomers))
+    });
+
+    it('should be a function', () => {
+      expect(customer).to.be.a('function');
+    });
+
+    it('should be and instance of User', () => {
+      expect(customer).to.be.an.instanceof(Customer);
+    });
+
+    it('should have an id that is a number', () => {
+      expect(customer.id).to.be.a('number');
+    });
+
+    it('should have an id that is a positive number', () => {
+      expect(customer.id).to.be.greaterThan(0);
+    });
+
+    it('should have a name that is a string', () => {
+      expect(customer.name).to.be.a('string');
+    });
+})
